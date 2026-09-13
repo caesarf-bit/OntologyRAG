8d38ef0c8749a8d3cadfdfb896e2b2561da65468
Caesar Farah <caesarf@mail.uni-paderborn.de>
2026-09-13 14:01:23 +0200

    change db location

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from langchain_huggingface import HuggingFaceEmbeddings
 
 docs_dir = Path("./trimmed_docs")
-persist_directory = "dbv1/chroma_db"
+persist_directory = "db/dbv1/chroma_db"
 
 embedding_model = HuggingFaceEmbeddings(model_name="BAAI/bge-m3",
     model_kwargs={"device": "cpu"},
